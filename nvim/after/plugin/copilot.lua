3192461d440e7de798bd86eac8f7532c14291aae
Juraj Petráš <jurkopetras@gmail.com>
2023-01-08 23:43:23 +0100

    Changed copilot shortcut

--- nvim/after/plugin/copilot.lua
+++ nvim/after/plugin/copilot.lua
@@ -2,4 +2,4 @@ vim.g.copilot_node_command = "~/.nvm/versions/node/v16*/bin/node"
 vim.g.copilot_no_tab_map = true
 vim.g.copilot_assume_mapped = true
 
-vim.keymap.set("i", "<C-.>", "copilot#Accept()", { silent = true, expr = true })
+vim.keymap.set("i", "<C-u>", "copilot#Accept()", { silent = true, expr = true })
