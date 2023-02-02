6a701af9e568411c8664a08bebe915a11411f59f
Juraj Petráš <jurkopetras@gmail.com>
2023-02-02 11:25:00 +0100

    Ignore case when searching

--- nvim/lua/hackder/set.lua
+++ nvim/lua/hackder/set.lua
@@ -5,7 +5,6 @@ vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20"
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
--- Tab
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
@@ -20,6 +19,9 @@ vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
+-- Search
+
+vim.opt.ignorecase = true
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
