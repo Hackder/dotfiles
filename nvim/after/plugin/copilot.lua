0f1cc04921e523d1485003ebdf6671a572ef11f1
Juraj Petráš <jurkopetras@gmail.com>
2023-02-12 23:03:10 +0100

    Fixed copilot

--- nvim/after/plugin/copilot.lua
+++ nvim/after/plugin/copilot.lua
@@ -1,4 +1,4 @@
-vim.g.copilot_node_command = "~/.nvm/versions/node/v16*/bin/node"
+vim.g.copilot_node_command = "~/.nvm/versions/node/v16.19.*/bin/node"
 vim.g.copilot_no_tab_map = true
 vim.g.copilot_assume_mapped = true
 
