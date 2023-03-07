7503ac426ca5d73df4489f68b28bb9e3f765797c
Juraj Petráš <jurkopetras@gmail.com>
2023-03-07 11:57:19 +0100

    Use less for cht.sh output

--- scripts/tmux-cht.sh
+++ scripts/tmux-cht.sh
@@ -8,8 +8,8 @@ read -p "Enter Query: " query
 
 if grep -qs "$selected" ~/dotfiles/tmux/.tmux-cht-languages; then
     query=`echo $query | tr ' ' '+'`
-    tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+    tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query | less -r"
 else
-    tmux neww bash -c "curl -s cht.sh/$selected~$query & while [ : ]; do sleep 1; done"
+    tmux neww bash -c "curl -s cht.sh/$selected~$query | less -r"
 fi
 
