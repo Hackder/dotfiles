c7c1bf4fe8a3d2c03844ea81d2dc430f911ed17f
Juraj Petráš <jurkopetras@gmail.com>
2023-05-26 18:47:17 +0200

    fix: wsl clipboard

--- nvim/lua/hackder/set.lua
+++ nvim/lua/hackder/set.lua
@@ -40,6 +40,20 @@ vim.opt.updatetime = 50
 
 vim.g.mapleader = " "
 
+-- Fix wsl clipboard
+vim.g.clipboard = {
+  name = "win32yank-wsl",
+  copy = {
+    ["+"] = "win32yank.exe -i --crlf",
+    ["*"] = "win32yank.exe -i --crlf",
+  },
+  paste = {
+    ["+"] = "win32yank.exe -o --lf",
+    ["*"] = "win32yank.exe -o --lf",
+  },
+  cache_enabled = 0,
+}
+
 -- Prettier
 vim.api.nvim_create_user_command(
   'Prettier',
