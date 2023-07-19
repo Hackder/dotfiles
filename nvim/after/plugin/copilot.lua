c4a9ca38905b4c08241ac9cb0423f6cefc991ee3
Juraj Petráš <jurkopetras@gmail.com>
2023-07-20 01:03:06 +0200

    fix: copilot random utf-8 characters

--- nvim/after/plugin/copilot.lua
+++ nvim/after/plugin/copilot.lua
@@ -2,4 +2,4 @@ vim.g.copilot_node_command = "node"
 vim.g.copilot_no_tab_map = true
 vim.g.copilot_assume_mapped = true
 
-vim.keymap.set("i", "<C-j>", "copilot#Accept()", { silent = true, expr = true })
+vim.keymap.set("i", "<C-j>", 'copilot#Accept("<CR>")', { noremap = true, silent = true, expr = true, replace_keycodes = false })
