0b8f3a33e1e77fbc970830b935409369728f7558
Juraj Petráš <jurkopetras@gmail.com>
2023-09-09 14:09:46 +0200

    feat: macbook paths

--- scripts/tmux-sessionizer.sh
+++ scripts/tmux-sessionizer.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/pmat ~/pmat/interactives/ ~/ ~/programming/ ~/temp/ ~/proxic/ ~/pollux/ -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$(find ~/pmat ~/pmat/interactives/ ~/ ~/programming/ ~/temp/ ~/proxic/ ~/pollux/ ~/dev ~/dev/pmat -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
 if [[ -z $selected ]]; then
