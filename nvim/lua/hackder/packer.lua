c5b799e6a3384b6a9ea64d522b471e73e6fc7e6e
Juraj Petráš <jurkopetras@gmail.com>
2023-09-19 20:23:37 +0200

    feat: treesitter textobjects

--- nvim/lua/hackder/packer.lua
+++ nvim/lua/hackder/packer.lua
@@ -20,6 +20,11 @@ return require('packer').startup(function(use)
 
   use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
   use('nvim-treesitter/nvim-treesitter-context')
+  use({
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    after = "nvim-treesitter",
+    requires = "nvim-treesitter/nvim-treesitter",
+  })
   use('ThePrimeagen/harpoon')
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
