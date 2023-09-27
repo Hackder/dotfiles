e9be9675b1b8510ca2f0dee4fdec4d19246ae21a
Juraj Petráš <jurkopetras@gmail.com>
2023-09-27 11:40:22 +0200

    feat: do not display error output from find in sessionizer

--- scripts/tmux-sessionizer.sh
+++ scripts/tmux-sessionizer.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/pmat ~/pmat/interactives/ ~/ ~/programming/ ~/temp/ ~/proxic/ ~/pollux/ ~/dev ~/dev/pmat ~/school -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$(find ~/pmat ~/pmat/interactives/ ~/ ~/programming/ ~/temp/ ~/proxic/ ~/pollux/ ~/dev ~/dev/pmat ~/school -mindepth 1 -maxdepth 1 -type d 2>/dev/null | fzf)
 fi
 
 if [[ -z $selected ]]; then
