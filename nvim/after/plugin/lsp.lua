a1af2ac7386f55426abae0cf1dec4239444865ec
Juraj Petráš <jurkopetras@gmail.com>
2024-01-04 01:20:01 +0100

    feat: format TS diagnostics

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -7,6 +7,46 @@ require('mason-lspconfig').setup({
   ensure_installed = { 'tsserver', 'eslint', 'rust_analyzer', 'lua_ls' },
   handlers = {
     lsp.default_setup,
+    tsserver = function ()
+      require("lspconfig").tsserver.setup({
+        handlers = {
+          ["textDocument/publishDiagnostics"] = function(
+            _,
+            result,
+            ctx,
+            config
+          )
+            if result.diagnostics == nil then
+              return
+            end
+
+            -- ignore some tsserver diagnostics
+            local idx = 1
+            while idx <= #result.diagnostics do
+              local entry = result.diagnostics[idx]
+
+              local formatter = require('format-ts-errors')[entry.code]
+              entry.message = formatter and formatter(entry.message) or entry.message
+
+              -- codes: https://github.com/microsoft/TypeScript/blob/main/src/compiler/diagnosticMessages.json
+              if entry.code == 80001 then
+                -- { message = "File is a CommonJS module; it may be converted to an ES module.", }
+                table.remove(result.diagnostics, idx)
+              else
+                idx = idx + 1
+              end
+            end
+
+            vim.lsp.diagnostic.on_publish_diagnostics(
+              _,
+              result,
+              ctx,
+              config
+            )
+          end,
+        },
+      })
+    end
   }
 })
 
--- nvim/lua/hackder/packer.lua
+++ nvim/lua/hackder/packer.lua
@@ -89,4 +89,8 @@ return require('packer').startup(function(use)
     'glacambre/firenvim',
     run = function() vim.fn['firenvim#install'](0) end
   }
+
+  use {
+    "davidosomething/format-ts-errors.nvim"
+  }
 end)
