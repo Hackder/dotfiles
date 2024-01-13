2ab3a477d3d6a195e62b30823c5f07765ef42e31
Juraj Petráš <jurkopetras@gmail.com>
2024-01-13 03:52:22 +0100

    feat: trouble remaps

--- nvim/lua/hackder/plugins/trouble.lua
+++ nvim/lua/hackder/plugins/trouble.lua
@@ -6,5 +6,11 @@ return {
     vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end, { desc = "Toggle trouble" })
     vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end, { desc = "Toggle trouble" })
     vim.keymap.set("n", "<leader>gR", function() require("trouble").toggle("lsp_references") end, { desc = "Toggle trouble" })
+    vim.keymap.set("n", "[d", function()
+     require("trouble").next({ skip_groups = true, jump = true })
+    end)
+    vim.keymap.set("n", "]d", function()
+      require("trouble").previous({ skip_groups = true, jump = true })
+    end)
   end
 }
