80bd4e65873b9a053e8e4d09060dfe7bac179449
Juraj Petráš <jurkopetras@gmail.com>
2024-01-14 01:24:22 +0100

    feat: colorscheme

--- nvim/lua/hackder/plugins/colorscheme.lua
+++ nvim/lua/hackder/plugins/colorscheme.lua
@@ -1,17 +1,56 @@
 return {
-  "folke/tokyonight.nvim",
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {},
+  --   config = function()
+  --     vim.cmd.colorscheme("tokyonight-night")
+  --   end
+  -- },
+  -- {
+  --   "rose-pine/neovim",
+  --   as = "rose-pine",
+  --   config = function()
+  --     require("rose-pine").setup({
+  --       disable_background = false,
+  --       styles = {
+  --         bold = false,
+  --       }
+  --     })
+  --
+  --     vim.cmd("colorscheme rose-pine")
+  --     -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  --     -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  --     -- vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+  --   end
+  -- },
+  -- {
+  --   "ellisonleao/gruvbox.nvim",
+  --   priority = 1000,
+  --   config = function()
+  --     require("gruvbox").setup({
+  --       contrast = "hard",
+  --       terminal_colors = false,
+  --       bold = false,
+  --     })
+  --     vim.o.background = "dark"
+  --     vim.cmd.colorscheme("gruvbox")
+  --     -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  --     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  --     -- vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+  --   end
+  -- }
   {
-    "rose-pine/neovim",
-    as = "rose-pine",
+    "RRethy/nvim-base16",
+    lazy = false,
+    priority = 1000,
     config = function()
-      require("rose-pine").setup({
-        disable_background = true
-      })
-
-      vim.cmd("colorscheme rose-pine")
-      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-      vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+      vim.g.base16colorspace = 256
+      vim.o.background = "dark"
+      vim.cmd.colorscheme("base16-gruvbox-dark-hard")
+      vim.cmd("syntax on")
+      vim.cmd("hi Normal ctermbg=none")
     end
   }
 }
