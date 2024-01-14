04ff5017aae2c85f02ae3c855ba4ac9d0962e84d
Juraj Petráš <jurkopetras@gmail.com>
2024-01-14 17:21:59 +0100

    fix: finding .env

--- nvim/lua/hackder/plugins/telescope.lua
+++ nvim/lua/hackder/plugins/telescope.lua
@@ -7,16 +7,17 @@ return {
       defaults = {
         file_ignore_patterns = { ".git", "node_modules" }
       },
-      pickers = {
-        find_files = { hidden = true, },
-      }
     })
 
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Search git files' })
     vim.keymap.set('n', '<leader>ff', builtin.git_files, { desc = 'Search git files ' })
     vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = 'Search diagnostics' })
-    vim.keymap.set('n', '<leader>fa', builtin.find_files, { desc = 'Search all files' })
+    vim.keymap.set('n', '<leader>fa', function()
+      builtin.find_files({
+        find_command = { 'rg', '--files', '--hidden', '--ignore', '-u', '--iglob', '!.git' },
+      })
+    end, { desc = 'Search all files' })
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Search all files' })
     vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = 'Grep string in current file' })
     vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Search buffers' })
