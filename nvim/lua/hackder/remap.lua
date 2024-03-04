2747b903f07c0cc0f22f2dde6597183d9a459fed
Juraj Petráš <jurkopetras@gmail.com>
2024-03-04 01:56:37 +0100

    chore: format

--- nvim/lua/hackder/remap.lua
+++ nvim/lua/hackder/remap.lua
@@ -1,33 +1,44 @@
-vim.keymap.set("n", "<leader>e", vim.cmd.Ex, { desc = 'Open NetRW' })
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = 'Move selection down' })
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = 'Move selection up' })
+vim.keymap.set("n", "<leader>e", vim.cmd.Ex, { desc = "Open NetRW" })
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
 
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = 'Scroll down half page' })
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = 'Scroll up half page' })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down half page" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up half page" })
 
-vim.keymap.set("x", "<leader>p", "\"_dP", { desc = 'Paste without yanking' })
+vim.keymap.set("x", "<leader>p", '"_dP', { desc = "Paste without yanking" })
 
-vim.keymap.set("n", "<leader>y", "\"+y", { desc = 'Copy to clipboard' })
-vim.keymap.set("v", "<leader>y", "\"+y", { desc = 'Copy to clipboard' })
-vim.keymap.set("n", "<leader>Y", "\"+Y", { desc = 'Copy to clipboard' })
+vim.keymap.set("n", "<leader>y", '"+y', { desc = "Copy to clipboard" })
+vim.keymap.set("v", "<leader>y", '"+y', { desc = "Copy to clipboard" })
+vim.keymap.set("n", "<leader>Y", '"+Y', { desc = "Copy to clipboard" })
 
-vim.keymap.set("n", "<leader>d", "\"_d", { desc = 'Delete without yanking' })
-vim.keymap.set("v", "<leader>d", "\"_d", { desc = 'Delete without yanking' })
+vim.keymap.set("n", "<leader>d", '"_d', { desc = "Delete without yanking" })
+vim.keymap.set("v", "<leader>d", '"_d', { desc = "Delete without yanking" })
 
-vim.keymap.set("i", "<C-c>", "<Esc>", { desc = 'Exit insert mode' })
+vim.keymap.set("i", "<C-c>", "<Esc>", { desc = "Exit insert mode" })
 
 vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<leader>lf", function() vim.lsp.buf.format() end, { desc = 'Format document using LSP' })
+vim.keymap.set("n", "<leader>lf", function()
+	vim.lsp.buf.format()
+end, { desc = "Format document using LSP" })
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = 'Rename current word' })
+vim.keymap.set(
+	"n",
+	"<leader>s",
+	[[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+	{ desc = "Rename current word" }
+)
 
-vim.keymap.set("n", "<F11>", "<cmd>let g:neovide_fullscreen = !g:neovide_fullscreen<cr>",
-  { desc = 'Toggle fullscreen in neovide' })
+vim.keymap.set(
+	"n",
+	"<F11>",
+	"<cmd>let g:neovide_fullscreen = !g:neovide_fullscreen<cr>",
+	{ desc = "Toggle fullscreen in neovide" }
+)
 
-vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = 'Exit terminal mode' })
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
-vim.keymap.set("n", "<C-n>", ":cprevious<CR>", { desc = 'Previous item in quickfix list' })
-vim.keymap.set("n", "<C-m>", ":cnext<CR>", { desc = 'Next item in quickfix list' })
+vim.keymap.set("n", "<C-n>", ":cprevious<CR>", { desc = "Previous item in quickfix list" })
+vim.keymap.set("n", "<C-m>", ":cnext<CR>", { desc = "Next item in quickfix list" })
 
-vim.keymap.set("v", ">", ">gv", { desc = 'Indent selection without losing selection' })
-vim.keymap.set("v", "<", "<gv", { desc = 'Unindent selection without losing selection' })
+vim.keymap.set("v", ">", ">gv", { desc = "Indent selection without losing selection" })
+vim.keymap.set("v", "<", "<gv", { desc = "Unindent selection without losing selection" })
