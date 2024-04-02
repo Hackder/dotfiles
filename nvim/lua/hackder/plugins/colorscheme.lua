2504ccd2e2a59c9b9cde89337600e9376fdc7cf9
Juraj Petráš <jurkopetras@gmail.com>
2024-04-02 15:06:57 +0200

    chore: removed commented old colorschemes

--- nvim/lua/hackder/plugins/colorscheme.lua
+++ nvim/lua/hackder/plugins/colorscheme.lua
@@ -1,46 +1,4 @@
 return {
-	-- {
-	--   "folke/tokyonight.nvim",
-	--   lazy = false,
-	--   priority = 1000,
-	--   opts = {},
-	--   config = function()
-	--     vim.cmd.colorscheme("tokyonight-night")
-	--   end
-	-- },
-	-- {
-	--   "rose-pine/neovim",
-	--   as = "rose-pine",
-	--   config = function()
-	--     require("rose-pine").setup({
-	--       disable_background = false,
-	--       styles = {
-	--         bold = false,
-	--       }
-	--     })
-	--
-	--     vim.cmd("colorscheme rose-pine")
-	--     -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	--     -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-	--     -- vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
-	--   end
-	-- },
-	-- {
-	--   "ellisonleao/gruvbox.nvim",
-	--   priority = 1000,
-	--   config = function()
-	--     require("gruvbox").setup({
-	--       contrast = "hard",
-	--       terminal_colors = false,
-	--       bold = false,
-	--     })
-	--     vim.o.background = "dark"
-	--     vim.cmd.colorscheme("gruvbox")
-	--     -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	--     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-	--     -- vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
-	--   end
-	-- },
 	{
 		"RRethy/nvim-base16",
 		lazy = false,
@@ -77,35 +35,4 @@ return {
 			vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
 		end,
 	},
-	-- {
-	--   'echasnovski/mini.base16',
-	--   lazy = false,
-	--   priority = 1000,
-	--   version = '*',
-	--   config = function()
-	--     require('mini.base16').setup({
-	--       palette = {
-	--         base00 = '#1d2021',
-	--         base01 = '#3c3836',
-	--         base02 = '#504945',
-	--         base03 = '#665c54',
-	--         base04 = '#bdae93',
-	--         base05 = '#d5c4a1',
-	--         base06 = '#ebdbb2',
-	--         base07 = '#fbf1c7',
-	--         base08 = '#fb4934',
-	--         base09 = '#fe8019',
-	--         base0A = '#fabd2f',
-	--         base0B = '#b8bb26',
-	--         base0C = '#8ec07c',
-	--         base0D = '#83a598',
-	--         base0E = '#d3869b',
-	--         base0F = '#d65d0e',
-	--       },
-	--       plugins = {
-	--         default = true,
-	--       },
-	--     })
-	--   end
-	-- },
 }
