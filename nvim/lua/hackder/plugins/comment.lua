58e4b569669cdb3d2eb3b04b15b38db75750f78c
Juraj Petráš <jurkopetras@gmail.com>
2024-07-28 10:51:22 +0200

    feat: jsx comments

--- nvim/lua/hackder/plugins/comment.lua
+++ nvim/lua/hackder/plugins/comment.lua
@@ -1,8 +1,13 @@
 return {
 	{
 		"numToStr/Comment.nvim",
+		dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
 		config = function()
-			require("Comment").setup()
+			local comment = require("Comment")
+			local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
+			comment.setup({
+				pre_hook = ts_context_commentstring.create_pre_hook(),
+			})
 			vim.keymap.set("n", "<leader>/", function()
 				require("Comment.api").toggle.linewise.current()
 			end, { desc = "Comment line" })
