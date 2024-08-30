fcc1f9eacafd5d39658c6e7d2a8ed6538de6484a
Juraj Petráš <jurkopetras@gmail.com>
2024-08-31 01:16:24 +0200

    feat: window resizing

--- nvim/lua/hackder/remap.lua
+++ nvim/lua/hackder/remap.lua
@@ -41,3 +41,15 @@ vim.keymap.set("n", "<C-m>", ":cnext<CR>", { desc = "Next item in quickfix list"
 
 vim.keymap.set("v", ">", ">gv", { desc = "Indent selection without losing selection" })
 vim.keymap.set("v", "<", "<gv", { desc = "Unindent selection without losing selection" })
+
+-- Resize window with Ctrl + Left
+vim.api.nvim_set_keymap("n", "<C-Left>", ":vertical resize -2<CR>", { noremap = true, silent = true })
+
+-- Resize window with Ctrl + Right
+vim.api.nvim_set_keymap("n", "<C-Right>", ":vertical resize +2<CR>", { noremap = true, silent = true })
+
+-- Resize window with Ctrl + Up
+vim.api.nvim_set_keymap("n", "<C-Up>", ":resize +2<CR>", { noremap = true, silent = true })
+
+-- Resize window with Ctrl + Down
+vim.api.nvim_set_keymap("n", "<C-Down>", ":resize -2<CR>", { noremap = true, silent = true })
