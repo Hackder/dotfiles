8bae154182640bd7b2dd04a8d86fbeac9f340fb6
Juraj Petráš <jurkopetras@gmail.com>
2024-09-07 16:14:44 +0200

    feat: init conda

--- workstation.sh
+++ workstation.sh
@@ -14,6 +14,7 @@ if ! command -v git &> /dev/null; then
   fi
   bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
   rm ~/miniconda3/miniconda.sh
+  ~/miniconda3/bin/conda init bash
   ~/miniconda3/bin/conda install -y git
 fi
 
