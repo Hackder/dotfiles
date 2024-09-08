defdca6ee7b02230ea09245683819cf00d64dcdd
Juraj Petráš <jurkopetras@gmail.com>
2024-09-09 01:37:01 +0200

    feat: hammerspoon

---
+++ hammerspoon/.hammerspoon/init.lua
@@ -0,0 +1,21 @@
+hs.hotkey.bind({ "alt" }, "J", function()
+	local app = hs.application.find("Arc")
+	if app then
+		local windows = app:allWindows()
+		local leftMostWindow = windows[1]
+
+		for i, window in ipairs(windows) do
+			if window:frame().x < leftMostWindow:frame().x then
+				leftMostWindow = window
+			end
+		end
+
+		leftMostWindow:focus()
+		leftMostWindow:focus()
+		leftMostWindow:focus()
+	end
+end)
+
+hs.hotkey.bind({ "alt" }, "H", function()
+	hs.application.launchOrFocus("Kitty")
+end)
