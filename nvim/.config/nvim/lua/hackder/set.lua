01338c0fda07784d8ed7d77aae6b79e7fea71edc
Juraj Petráš <jurkopetras@gmail.com>
2024-10-14 10:05:19 +0200

    feat: 4 tab indentation for C

--- nvim/.config/nvim/lua/hackder/set.lua
+++ nvim/.config/nvim/lua/hackder/set.lua
@@ -64,7 +64,7 @@ end
 
 -- File specific tabstop and shiftwidth
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = "cpp",
+	pattern = { "cpp", "c" },
 	callback = function()
 		vim.opt_local.shiftwidth = 4
 		vim.opt_local.tabstop = 4
