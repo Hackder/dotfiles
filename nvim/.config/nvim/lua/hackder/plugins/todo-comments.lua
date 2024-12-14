a43ea0caeb3623a959f5250ac817121244aeb455
Juraj Petráš <jurkopetras@gmail.com>
2024-12-14 01:26:37 +0100

    fix: todo-comments for hidden files

--- nvim/.config/nvim/lua/hackder/plugins/todo-comments.lua
+++ nvim/.config/nvim/lua/hackder/plugins/todo-comments.lua
@@ -1,9 +1,24 @@
 return {
 	{
 		"folke/todo-comments.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
+		dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
 		opts = {
-			search = { pattern = [[\b(KEYWORDS)(\([^\)]*\))?:]] },
+			search = {
+				command = "rg",
+				args = {
+					"--hidden",
+					"--glob",
+					"!\\.git",
+					"--glob",
+					"!node_modules",
+					"--color=never",
+					"--no-heading",
+					"--with-filename",
+					"--line-number",
+					"--column",
+				},
+				pattern = [[\b(KEYWORDS)(\([^\)]*\))?:]],
+			},
 			highlight = {
 				pattern = [[.*<((KEYWORDS)%(\(.{-1,}\))?):]],
 			},
