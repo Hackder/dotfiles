778c0abb02c8be4efbe4ecf4c41c5b6666df4f61
Juraj Petráš <jurkopetras@gmail.com>
2024-12-14 01:19:52 +0100

    feat: highlight todo comments with names

--- nvim/.config/nvim/lua/hackder/plugins/todo-comments.lua
+++ nvim/.config/nvim/lua/hackder/plugins/todo-comments.lua
@@ -2,6 +2,11 @@ return {
 	{
 		"folke/todo-comments.nvim",
 		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = {},
+		opts = {
+			search = { pattern = [[\b(KEYWORDS)(\([^\)]*\))?:]] },
+			highlight = {
+				pattern = [[.*<((KEYWORDS)%(\(.{-1,}\))?):]],
+			},
+		},
 	},
 }
