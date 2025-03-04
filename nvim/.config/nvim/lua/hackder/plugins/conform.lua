0123570dd1c66dc70c5f790591174d8448b632b6
Juraj Petráš <jurkopetras@gmail.com>
2025-03-05 00:26:04 +0100

    feat: format json with prettier

--- nvim/.config/nvim/lua/hackder/plugins/conform.lua
+++ nvim/.config/nvim/lua/hackder/plugins/conform.lua
@@ -26,6 +26,7 @@ return {
 					javascriptreact = { "prettierd", "prettier", stop_after_first = true },
 					typescript = { "prettierd", "prettier", stop_after_first = true },
 					typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+					json = { "prettierd", "prettier", stop_after_first = true },
 					vue = { "prettierd", "prettier", stop_after_first = true },
 					astro = { "prettierd", "prettier", stop_after_first = true },
 					java = { "google-java-format" },
