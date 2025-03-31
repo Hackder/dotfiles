a357ca7bb3aec2de999e6c7ce105c7bfad785f93
Juraj Petráš <jurkopetras@gmail.com>
2025-03-31 15:14:27 +0200

    feat: use copilot in split

--- nvim/.config/nvim/lua/hackder/plugins/copilot.lua
+++ nvim/.config/nvim/lua/hackder/plugins/copilot.lua
@@ -69,9 +69,9 @@ return {
 		opts = {
 			chat_autocomplete = false,
 			window = {
-				layout = "float", -- 'vertical', 'horizontal', 'float', 'replace'
-				width = 0.75, -- fractional width of parent, or absolute width in columns when > 1
-				height = 0.75, -- fractional height of parent, or absolute height in rows when > 1
+				layout = "vertical", -- 'vertical', 'horizontal', 'float', 'replace'
+				-- width = 0.75, -- fractional width of parent, or absolute width in columns when > 1
+				-- height = 0.75, -- fractional height of parent, or absolute height in rows when > 1
 			},
 			mappings = {
 				complete = {
