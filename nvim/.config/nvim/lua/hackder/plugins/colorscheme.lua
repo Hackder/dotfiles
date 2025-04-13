55bab750c59e8604d1dadbc66fd1981755541fd1
Juraj Petráš <jurkopetras@gmail.com>
2025-04-13 10:45:37 +0200

    feat: light mode

--- nvim/.config/nvim/lua/hackder/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/hackder/plugins/colorscheme.lua
@@ -35,5 +35,10 @@ return {
 			-- vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
 		end,
 	},
-	{ "catppuccin/nvim", lazy = false, name = "catppuccin", priority = 1000 },
+	{
+		"projekt0n/github-nvim-theme",
+		name = "github-theme",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+	},
 }
--- nvim/.config/nvim/lua/hackder/plugins/lualine.lua
+++ nvim/.config/nvim/lua/hackder/plugins/lualine.lua
@@ -15,7 +15,7 @@ return {
 			require("lualine").setup({
 				options = {
 					icons_enabled = true,
-					theme = custom_gruvbox,
+					theme = "auto",
 					component_separators = { left = "|", right = "|" },
 					section_separators = { left = "", right = "" },
 					disabled_filetypes = {
