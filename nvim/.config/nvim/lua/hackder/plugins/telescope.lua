5f8799cb4e48959abadf3268a2b085e149a02590
Juraj Petráš <jurkopetras@gmail.com>
2025-09-23 21:34:19 +0200

    feat: fzf prefer filename

--- nvim/.config/nvim/lua/hackder/plugins/telescope.lua
+++ nvim/.config/nvim/lua/hackder/plugins/telescope.lua
@@ -41,13 +41,12 @@ local git_command = {
 
 return {
 	{
-		"nvim-telescope/telescope-fzf-native.nvim",
-		build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+		"natecraddock/telescope-zf-native.nvim",
 	},
 	{
 		"nvim-telescope/telescope.nvim",
 		branch = "0.1.x",
-		dependencies = { "nvim-lua/plenary.nvim", { "nvim-telescope/telescope-fzf-native.nvim" } },
+		dependencies = { "nvim-lua/plenary.nvim", "natecraddock/telescope-zf-native.nvim" },
 		config = function()
 			local open_with_trouble = require("trouble.sources.telescope").open
 
@@ -59,10 +58,15 @@ return {
 					},
 				},
 				extensions = {
-					fzf = {},
+					fzf = {
+						fuzzy = true,
+						override_generic_sorter = false,
+						override_file_sorter = true,
+						case_mode = "smart_case",
+					},
 				},
 			})
-			require("telescope").load_extension("fzf")
+			require("telescope").load_extension("zf-native")
 
 			local pickers = require("hackder.telescope-pickers")
 
