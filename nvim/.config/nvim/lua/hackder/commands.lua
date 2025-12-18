c40e207bbffbe92da428a23e994e5d10a8560eab
Juraj Petráš <jurkopetras@gmail.com>
2025-12-18 22:38:50 +0100

    feat: Rust analyzer target config

--- nvim/.config/nvim/lua/hackder/commands.lua
+++ nvim/.config/nvim/lua/hackder/commands.lua
@@ -2,3 +2,31 @@ vim.api.nvim_create_user_command("Open", function(opts)
 	local path = vim.fn.expand("%:p:h")
 	os.execute("open " .. path)
 end, {})
+
+vim.api.nvim_create_user_command("RustAnalyzerTarget", function(opts)
+	local target = opts.args
+	if not target or target == "" then
+		vim.notify("Please provide a target triple", vim.log.levels.ERROR)
+		return
+	end
+	local cmd = string.format("RustAnalyzer config { cargo = { target = '%s' } }", target)
+	vim.cmd(cmd)
+end, {
+	nargs = 1,
+	complete = function(ArgLead, CmdLine, CursorPos)
+		local list = vim.fn.systemlist("rustc --print target-list")
+		if type(list) ~= "table" then
+			return {}
+		end
+		if not ArgLead or ArgLead == "" then
+			return list
+		end
+		local matches = {}
+		for _, item in ipairs(list) do
+			if vim.startswith(item, ArgLead) then
+				table.insert(matches, item)
+			end
+		end
+		return matches
+	end,
+})
